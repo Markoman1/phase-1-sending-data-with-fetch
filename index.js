9665eefa66767414fab2ee1fb7dcc8fe46a53de0
Mark <mgalex12@icloud.com>
2022-07-25 14:40:32 -0400

    commit tree 3665d6c37665e20aa2ba4292ab70af30fe21f6e4

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+// Add your code here   
+function submitData(name, email) {
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({name: name, email: email})
+    }
+        fetch("http://localhost:3000/users", configurationObject)
+        .then(function (res) {
+            return res.json();
+        })
+        .then(function (obj) {
+            document.body.innerHTML = obj.id
+        })
+            .catch(function (error) {
+                alert("Bad things! Ragnarők!");
+            })
+    
+}
